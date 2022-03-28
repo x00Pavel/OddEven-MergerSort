1ea5abee7666b2ff99e85d8d8b8fd6d2ea9426a3
Pavel Yadlouski <pyadlous@redhat.com>
2022-03-28 15:55:08 +0200

    Update script for execution

--- test.sh
+++ test.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-#pocet cisel bud zadam nebo 10 :)
-if [ $# -lt 1 ];then 
-    numbers=10;
-else
-    numbers=$1;
-fi;
+PREFIX=""
+
+# #pocet cisel bud zadam nebo 10 :)
+# if [ $# -lt 1 ];then 
+#     numbers=8;
+# else
+#     numbers=$1;
+# fi;
 
 #preklad cpp zdrojaku
-mpic++ --prefix /usr/local/share/OpenMPI -o oems oems.cpp
+if [[ $(hostname) == *"merlin"* ]]
+then
+    PREFIX="--prefix /usr/local/share/OpenMPI"
+fi
+
+mpic++ ${PREFIX} -o oems oems.cpp
 
 
 #vyrobeni souboru s random cisly
-dd if=/dev/random bs=1 count=$numbers of=numbers
+dd if=/dev/random bs=1 count=8 of=numbers
 
 #spusteni
-mpirun --prefix /usr/local/share/OpenMPI -np $numbers oems
+PREFIX="${PREFIX} --oversubscribe"
+mpirun ${PREFIX} -np 20 oems
 
 #uklid
 rm -f oems numbers
